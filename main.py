5f7537e86b716f353f443e350984aae708d4e2da
ItaySagy <itaysagy15999@gmail.com>
2022-12-19 15:54:45 +0200

    added a State to learn csv

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ all_states = data.state.to_list()
 guessed_state = []
 while len(guessed_state) < 50 :
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 states currect", prompt="Whats another state's name? ").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_state:
+                missing_states.append(state)
+                new_data = pandas.DataFrame(missing_states)
+                new_data.to_csv("states_to_learn.csv")
+        # print(missing_states)
+        break
     if answer_state in all_states:
         if answer_state not in guessed_state:
             guessed_state.append(answer_state)
